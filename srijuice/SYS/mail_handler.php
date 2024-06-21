37ec90829836d4ffad55fd83a8b6bc6e095c81e6
pavi <rkpavi06@gmail.com>
2024-06-21 21:06:04 +0530

    emoved background email since exec() is blocked by many free hosting providers

--- srijuice/SYS/mail_handler.php
+++ srijuice/SYS/mail_handler.php
@@ -1,12 +1,41 @@
 <?php
+// mail_sender.php
+
+require 'vendor/autoload.php'; 
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
 function sendEmail($recipientEmail, $recipientName, $subject, $body) {
-    $recipientEmail = escapeshellarg($recipientEmail);
-    $recipientName = escapeshellarg($recipientName);
-    $subject = escapeshellarg($subject);
-    $body = escapeshellarg($body);
+    $smtp_host= "";
+    $smtp_port="587";
+    $mail_username="";
+    $mail_password= "";
+    $mail = new PHPMailer(true);
 
-    $command = "php mail_sender.php $recipientEmail $recipientName $subject $body > /dev/null &";
-    exec($command);
+
+    $mail = new PHPMailer(true);
+
+    try {
+        $mail->isSMTP(); 
+        $mail->Host = $smtp_host; 
+        $mail->SMTPAuth = true; 
+        $mail->Username = $mail_username; 
+        $mail->Password = $mail_password;  
+        $mail->SMTPSecure = 'tls'; 
+        $mail->Port = $smtp_port; 
+
+        $mail->setFrom('srijuic@lk.rk', 'srijuice');
+        $mail->addAddress($recipientEmail, $recipientName); 
+
+        $mail->isHTML(true); 
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+
+        $mail->send();
+        return 'Email has been sent successfully';
+    } catch (Exception $e) {
+        return 'Email could not be sent. Mailer Error: ' . $mail->ErrorInfo;
+    }
 }
-
 ?>
